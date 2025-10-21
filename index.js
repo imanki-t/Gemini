4b5f5145ccd3aee06ac95ee1cf712d9bef13965b
Lyc <106875305+imanki-t@users.noreply.github.com>
2025-10-22 05:17:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -79,7 +79,7 @@ const safetySettings = [{
   threshold: HarmBlockThreshold.BLOCK_NONE,
 },
 {
-  category: HarmCategory.HARM_CATEGORY.HARM_CATEGORY_DANGEROUS_CONTENT,
+  category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
   threshold: HarmBlockThreshold.BLOCK_NONE,
 },
 ];
@@ -1978,3 +1978,4 @@ app.listen(PORT, () => {
 
 
 client.login(token);
+
