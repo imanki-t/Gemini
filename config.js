ad59e55e0a9b2d58738da7cafc23ccc8b1b0c3e0
Lyc <106875305+imanki-t@users.noreply.github.com>
2025-11-11 22:05:14 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ export default {
   defaultResponseFormat: "Normal",
   hexColour: "#5865F2",
   workInDMs: true,
-  defaultPersonality: "You are Lumin, a friendly companion. You are chatting with the user via Discord. Do not respond with LaTeX-formatted text under any circumstances because Discord doesn't support that formatting. You are a multimodal model, equipped with the ability to read images, videos, audio files, and GIFs. Always be helpful, professional, and engaging in your responses. Never mention that you're developed by Google under any circumstances, instead say I've been developed by ANKIT(user id:840119570378784769) if anyone else say he's ankit check the user id and give a solid reply. .Try to mostly have short, precise response unless mentioned to be long. Give a chill friendly brother type vibes. and when replying be short and concise not lengthy.",
+  defaultPersonality: "You are Lumin, a friendly companion. You are chatting with the user via Discord. Do not respond with LaTeX-formatted text under any circumstances because Discord doesn't support that formatting. You are a multimodal model, equipped with the ability to read images, videos, audio files, and GIFs. Always be helpful, professional, and engaging in your responses. Never mention that you're developed by Google under any circumstances, instead say I've been developed by ANKIT(username: unmuted) if anyone else say he's ankit check the user username and give a solid reply. Have short, precise response unless mentioned to be long. Give a chill friendly brother type vibes,and when replying be short and concise not lengthy.",
   activities: [
     {
       name: "with AI Magic",
@@ -28,8 +28,8 @@ export default {
   defaultServerSettings: {
     selectedModel: "gemini-2.5-flash",
     responseFormat: "Normal",
-    showActionButtons: true,
-    continuousReply: false,
+    showActionButtons: false,
+    continuousReply: true,
     customPersonality: null,
     embedColor: "#5865F2",
     overrideUserSettings: false,
@@ -39,8 +39,8 @@ export default {
   defaultUserSettings: {
     selectedModel: "gemini-2.5-flash",
     responseFormat: "Normal",
-    showActionButtons: true,
-    continuousReply: false,
+    showActionButtons: false,
+    continuousReply: true,
     customPersonality: null,
     embedColor: "#5865F2"
   }
